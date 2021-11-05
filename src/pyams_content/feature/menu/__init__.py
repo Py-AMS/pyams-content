75955139c5bfcf97f3dc9a20a5d8c47177168dd1
Thierry Florac <thierry.florac@onf.fr>
2021-11-06 00:05:57 +0100

    Initialized menu feature

---
+++ src/pyams_content/feature/menu/__init__.py
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2015-2021 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""PyAMS_content.features.menu module
+
+Menus are very common navigation items which allows to navigate over specific sections of
+your website.
+
+A menu is generally a container for navigation items (ie internal or external links).
+"""
+
+__docformat__ = 'restructuredtext'
