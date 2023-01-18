285b2bafabc9adc3ba83979e7a08febfadee8949
Thierry Florac <thierry.florac@onf.fr>
2023-01-18 23:29:53 +0100

    PyAMS_portal updates for header and footer management

--- src/pyams_content/shared/common/portal.py
+++ src/pyams_content/shared/common/portal.py
@@ -19,6 +19,7 @@ from pyams_content.shared.common.interfaces import IBaseContentPortalContext, \
     ISharedContentPortalPage
 from pyams_portal.interfaces import IPortalPage, PORTAL_PAGE_KEY
 from pyams_portal.page import PortalPage
+from pyams_portal.utils import get_portal_page
 from pyams_utils.adapter import adapter_config, get_annotation_adapter
 from pyams_utils.factory import factory_config
 from pyams_utils.traversing import get_parent
@@ -32,7 +33,8 @@ class SharedContentPortalPageMixin:
 
     @volatile_property
     def can_inherit(self):
-        return IPortalPage(self.parent).template is not None
+        page = get_portal_page(self.parent, page_name=self.name)
+        return page.template is not None
 
     @property
     def parent(self):
@@ -50,6 +52,30 @@ class SharedContentPortalPage(SharedContentPortalPageMixin, PortalPage):
 
 @adapter_config(required=IBaseContentPortalContext,
                 provides=IPortalPage)
-def shared_content_portal_page_adapter(context):
+def shared_content_portal_page(context, page_name=''):
     """Shared content portal page adapter"""
-    return get_annotation_adapter(context, PORTAL_PAGE_KEY, ISharedContentPortalPage)
+
+    def set_page_name(page):
+        """Set page name after creation"""
+        page.name = page_name
+
+    key = f'{PORTAL_PAGE_KEY}::{page_name}' if page_name else PORTAL_PAGE_KEY
+    return get_annotation_adapter(context, key, ISharedContentPortalPage,
+                                  name=f'++page++{page_name}',
+                                  callback=set_page_name)
+
+
+@adapter_config(name='header',
+                required=IBaseContentPortalContext,
+                provides=IPortalPage)
+def shared_content_portal_page_header(context):
+    """Shared content portal page header adapter"""
+    return shared_content_portal_page(context, page_name='header')
+
+
+@adapter_config(name='footer',
+                required=IBaseContentPortalContext,
+                provides=IPortalPage)
+def shared_content_portal_page_footer(context):
+    """Shared content portal page footer adapter"""
+    return shared_content_portal_page(context, page_name='footer')
