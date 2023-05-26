3f8b72332ae6b2049f069b52d63b815aaac5ce36
Thierry Florac <tflorac@ulthar.net>
2023-05-26 21:59:00 +0200

    Added typed shared content custom info edit form

--- src/pyams_content/shared/common/zmi/types/content.py
+++ src/pyams_content/shared/common/zmi/types/content.py
@@ -16,8 +16,15 @@
 
 __docformat__ = 'restructuredtext'
 
+from pyramid.decorator import reify
+from zope.interface import Interface
+
+from pyams_content.shared.common.interfaces import ISharedTool
 from pyams_content.shared.common.interfaces.types import IWfTypedSharedContent
 from pyams_content.shared.common.zmi import SharedContentPropertiesEditForm
+from pyams_form.field import Fields
+from pyams_utils.registry import get_utility
+from pyams_zmi.form import AdminEditForm
 
 
 class TypedSharedContentPropertiesEditForm(SharedContentPropertiesEditForm):
@@ -26,3 +33,29 @@ class TypedSharedContentPropertiesEditForm(SharedContentPropertiesEditForm):
     interface = IWfTypedSharedContent
     fieldnames = ('title', 'short_name', 'content_url', 'data_type',
                   'header', 'description', 'notepad')
+
+
+class TypedSharedContentCustomInfoEditForm(AdminEditForm):
+    """Typed shared content custom information edit form"""
+
+    @reify
+    def datatype(self):
+        """Form context datatype getter"""
+        context = self.context
+        if IWfTypedSharedContent.providedBy(context):
+            return context.get_data_type()
+        return None
+
+    def get_content(self):
+        """Form content getter"""
+        manager = get_utility(ISharedTool, name=self.context.content_type)
+        return manager.shared_content_info_factory(self.context)
+
+    @property
+    def fields(self):
+        """Form fields getter"""
+        datatype = self.datatype
+        if datatype is None:
+            return Fields(Interface)
+        manager = get_utility(ISharedTool, name=self.context.content_type)
+        return Fields(manager.shared_content_info_factory).select(*datatype.field_names)
