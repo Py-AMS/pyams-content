c23657e1a27e3b2f3fbb208786de82c9a49145c9
Thierry Florac <tflorac@ulthar.net>
2023-08-26 00:37:01 +0200

    Added support for user params

--- src/pyams_content/shared/view/query.py
+++ src/pyams_content/shared/view/query.py
@@ -27,8 +27,8 @@ from pyams_catalog.query import CatalogResultSet, or_
 from pyams_content.shared.view import IViewQuery, IWfView
 from pyams_content.shared.view.interfaces import RELEVANCE_ORDER
 from pyams_content.shared.view.interfaces.query import END_PARAMS_MARKER, IViewQueryFilterExtension, \
-    IViewQueryParamsExtension
-from pyams_utils.adapter import ContextAdapter, adapter_config
+    IViewQueryParamsExtension, IViewUserQuery
+from pyams_utils.adapter import ContextAdapter, adapter_config, get_adapter_weight
 from pyams_utils.list import unique_iter
 from pyams_utils.registry import get_pyramid_registry, get_utility
 from pyams_utils.timezone import tztime
@@ -63,7 +63,7 @@ class ViewQuery(ContextAdapter):
         # check custom extensions
         get_all_params = True
         for name, adapter in sorted(registry.getAdapters((view,), IViewQueryParamsExtension),
-                                    key=lambda x: x[1].weight):
+                                    key=get_adapter_weight):
             for new_params in adapter.get_params(context, request):
                 if new_params is None:
                     return None
@@ -130,6 +130,27 @@ class ViewQuery(ContextAdapter):
             total_count = query[0]
             items = CatalogResultSet(query)
         for name, adapter in sorted(registry.getAdapters((view,), IViewQueryFilterExtension),
-                                    key=lambda x: x[1].weight):
+                                    key=get_adapter_weight):
             items = adapter.filter(context, items, request)
         return unique_iter(items), total_count, {}
+
+
+@adapter_config(name='user-params',
+                required=IWfView,
+                provides=IViewQueryParamsExtension)
+class UserViewQueryParamsExtension(ContextAdapter):
+    """User view query params extension"""
+
+    weight = 999
+
+    def __new__(cls, context):
+        if not context.allow_user_params:
+            return None
+        return ContextAdapter.__new__(cls)
+
+    def get_params(self, context, request=None):
+        """User params getter"""
+        registry = get_pyramid_registry()
+        for name, adapter in sorted(registry.getAdapters((self.context,), IViewUserQuery),
+                                    key=get_adapter_weight):
+            yield from adapter.get_user_params(request)
