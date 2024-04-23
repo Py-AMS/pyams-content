5ec11de992cf28f0429cf4c0708160f220ee9735
Thierry Florac <tflorac@ulthar.net>
2024-04-23 23:24:26 +0200

    Updated illustrations adapters

--- src/pyams_content/component/illustration/skin/__init__.py
+++ src/pyams_content/component/illustration/skin/__init__.py
@@ -18,11 +18,12 @@ This module provides base illustrations adapters.
 from zope.interface import Interface
 
 from pyams_content.component.illustration import IBaseIllustrationTarget, IIllustration, \
-    ILinkIllustration, ILinkIllustrationTarget
+    IIllustrationTargetBase, ILinkIllustration, ILinkIllustrationTarget
 from pyams_content.component.links import IInternalLink
 from pyams_content.interfaces import IBaseContent
 from pyams_content.shared.common import ISharedContent
-from pyams_content.skin.interfaces import IContentBannerIllustration, IContentNavigationIllustration
+from pyams_content.skin.interfaces import IContentBannerIllustration, IContentIllustration, \
+    IContentNavigationIllustration
 from pyams_layer.interfaces import IPyAMSLayer
 from pyams_portal.interfaces import HIDDEN_RENDERER_NAME
 from pyams_site.interfaces import ISiteRoot
@@ -34,11 +35,37 @@ from pyams_utils.interfaces.tales import ITALESExtension
 # Illustrations adapters
 #
 
-@adapter_config(required=(IInternalLink, IPyAMSLayer),
-                provides=IContentNavigationIllustration)
+@adapter_config(required=(IIllustrationTargetBase, IPyAMSLayer),
+                provides=IContentIllustration)
+def base_content_illustration(context, request):
+    """Base content illustration adapter"""
+    illustration = IIllustration(context, None)
+    if illustration and illustration.has_data():
+        return illustration
+    return None
+
+
+@adapter_config(name='pyams_illustration',
+                required=(Interface, Interface, Interface),
+                provides=ITALESExtension)
+class PyAMSIllustrationTALESExtension(ContextRequestViewAdapter):
+    """PyAMS illustration TALES extension"""
+
+    def render(self, context=None, name=''):
+        if context is None:
+            context = self.context
+        return self.request.registry.queryMultiAdapter((context, self.request),
+                                                       IContentIllustration,
+                                                       name=name)
+
+
+#
+# Navigation illustration adapters
+#
+
 @adapter_config(required=(IBaseIllustrationTarget, IPyAMSLayer),
                 provides=IContentNavigationIllustration)
-def base_content_navigation_illustration_factory(context, request):
+def base_content_navigation_illustration(context, request):
     """Default content navigation illustration adapter"""
     illustration = ILinkIllustration(context, None)
     if not (illustration and illustration.has_data()):
@@ -48,19 +75,25 @@ def base_content_navigation_illustration_factory(context, request):
             illustration = None
     if illustration and illustration.has_data():
         return illustration
-    if IInternalLink.providedBy(context):
-        target = context.get_target()
-        if target is not None:
-            illustration = request.registry.queryMultiAdapter((target, request),
-                                                              IContentNavigationIllustration)
-            if illustration and illustration.has_data():
-                return illustration
+    return None
+
+
+@adapter_config(required=(IInternalLink, IPyAMSLayer),
+                provides=IContentNavigationIllustration)
+def internal_link_navigation_illustration(context, request):
+    """Internal link navigation illustration adapter"""
+    target = context.get_target()
+    if target is not None:
+        illustration = request.registry.queryMultiAdapter((target, request),
+                                                          IContentNavigationIllustration)
+        if illustration and illustration.has_data():
+            return illustration
     return None
 
 
 @adapter_config(required=(ILinkIllustrationTarget, IPyAMSLayer),
                 provides=IContentNavigationIllustration)
-def link_content_navigation_illustration_factory(context, request):
+def link_content_navigation_illustration(context, request):
     """Content navigation illustration adapter for basic link illustration targets"""
     illustration = ILinkIllustration(context, None)
     if illustration and illustration.has_data():
@@ -69,7 +102,7 @@ def link_content_navigation_illustration_factory(context, request):
 
 @adapter_config(required=(ISharedContent, IPyAMSLayer),
                 provides=IContentNavigationIllustration)
-def shared_content_illustration_factory(context, request):
+def shared_content_navigation_illustration(context, request):
     """Shared content illustration factory"""
     version = context.visible_version
     if version is not None:
@@ -78,10 +111,10 @@ def shared_content_illustration_factory(context, request):
     return None
 
 
-@adapter_config(name='pyams_illustration',
+@adapter_config(name='pyams_navigation_illustration',
                 required=(Interface, Interface, Interface),
                 provides=ITALESExtension)
-class PyAMSIllustrationTALESExtension(ContextRequestViewAdapter):
+class PyAMSNavigationIllustrationTALESExtension(ContextRequestViewAdapter):
     """PyAMS navigation illustration TALES extension"""
 
     def render(self, context=None, name=''):
@@ -92,11 +125,15 @@ class PyAMSIllustrationTALESExtension(ContextRequestViewAdapter):
                                                        name=name)
 
 
+#
+# Banner illustration adapters
+#
+
 @adapter_config(required=(ISiteRoot, IPyAMSLayer),
                 provides=IContentBannerIllustration)
 @adapter_config(context=(IBaseContent, IPyAMSLayer),
                 provides=IContentBannerIllustration)
-def base_content_banner_illustration_factory(context, request):
+def base_content_banner_illustration(context, request):
     """Base content banner illustration adapter"""
     illustration = IIllustration(context, None)
     if illustration and illustration.has_data() and (illustration.renderer != HIDDEN_RENDERER_NAME):
