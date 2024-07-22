d223c251acea2c45c843debb64d068098b1b2136
Thierry Florac <tflorac@ulthar.net>
2024-07-22 03:11:39 +0200

    Check external links when creating or updating HTML paragraphs

--- src/pyams_content/component/paragraph/zmi/html.py
+++ src/pyams_content/component/paragraph/zmi/html.py
@@ -16,9 +16,11 @@ This module provides management interface components for raw source code and ric
 paragraphs.
 """
 
+from pyquery import PyQuery
 from pyramid.events import subscriber
-from zope.interface import alsoProvides
+from zope.interface import Invalid, alsoProvides
 
+from pyams_content.component.links.interfaces import IExternalLinksManagerInfo
 from pyams_content.component.paragraph import IParagraphContainer, IParagraphContainerTarget
 from pyams_content.component.paragraph.interfaces.html import HTML_PARAGRAPH_ICON_CLASS, \
     HTML_PARAGRAPH_NAME, HTML_PARAGRAPH_TYPE, IHTMLParagraph, IRawParagraph, \
@@ -26,7 +28,7 @@ from pyams_content.component.paragraph.interfaces.html import HTML_PARAGRAPH_ICO
 from pyams_content.component.paragraph.zmi import BaseParagraphAddForm, BaseParagraphAddMenu
 from pyams_content.component.paragraph.zmi.interfaces import IParagraphContainerBaseTable
 from pyams_form.ajax import ajax_form_config
-from pyams_form.interfaces.form import IFormUpdatedEvent
+from pyams_form.interfaces.form import IDataExtractedEvent, IFormUpdatedEvent
 from pyams_layer.interfaces import IPyAMSLayer
 from pyams_portal.interfaces import DEFAULT_RENDERER_NAME, HIDDEN_RENDERER_NAME
 from pyams_utils.interfaces.data import IObjectData
@@ -36,6 +38,8 @@ from pyams_zmi.interfaces.form import IPropertiesEditForm
 from pyams_zmi.interfaces.viewlet import IContextAddingsViewletManager
 
 __docformat__ = 'restructuredtext'
+
+from pyams_content import _
 
 
 #
@@ -113,3 +117,24 @@ class HTMLParagraphAddForm(BaseParagraphAddForm):
     """HTML rich text paragraph add form"""
 
     content_factory = IHTMLParagraph
+
+
+@subscriber(IDataExtractedEvent, form_selector=HTMLParagraphAddForm)
+@subscriber(IDataExtractedEvent, context_selector=IHTMLParagraph)
+def extract_paragraph_data(event):
+    """Extract paragraph data"""
+    form = event.form
+    request = form.request
+    settings = IExternalLinksManagerInfo(request.root, None)
+    if (settings is None) or not settings.check_external_links:
+        return
+    data = event.data
+    for lang, body in data.get('body', {}).items():
+        html = PyQuery(f'<html>{body}</html>')
+        for link in html('a[href]'):
+            href = link.attrib['href']
+            for host in settings.forbidden_hosts or ():
+                if host and href.startswith(host):
+                    form.widgets.errors += (Invalid(_("You can't create an external link to this site! "
+                                                      "Use an internal link instead...")),)
+                    return
